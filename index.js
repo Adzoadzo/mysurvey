c524161c7a03b2b1620ffdc9abd1921e85c99e43
adzoadzo <kotoriic@gmail.com>
2018-05-31 23:31:27 +0200

    login finished

--- index.js
+++ index.js
@@ -2,7 +2,39 @@ const express = require('express');
 const bodyparser = require("body-parser");
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const jwt_secret = 'WU5CjF8fHxG40S2t7oyk';
+
+var jwt    = require('jsonwebtoken');
 var MongoId = require('mongodb').ObjectID;
+var db;
+
+app.use('/', express.static('examples'));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+app.post('/login', function(request, response){
+    var user = request.body;
+  
+    db.collection("users").findOne({'username': user.username, 'password': user.password}, function(error, user) {
+      if (error){
+        throw error;
+      }else{
+        if(user){
+          var token = jwt.sign(user, jwt_secret, {
+            expiresIn: 20000 
+          });
+      
+          response.send({
+            success: true,
+            message: 'Authenticated',
+            token: token
+          })
+        }else{
+          response.status(401).send('Credentials are wrong.');
+        }
+      }
+    });
+  });
 
 app.use('/', express.static('examples'));
 app.use(express.json());       // to support JSON-encoded bodies
